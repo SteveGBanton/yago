83f10b31841a20ba279c421dfded064f67e932fa
Steve Banton <steve.g.banton@gmail.com>
2017-09-27 17:34:23 -0400

    Updated Meteor

--- imports/ui/components/EditSchemaForm/EditSchemaForm.jsx
+++ imports/ui/components/EditSchemaForm/EditSchemaForm.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Loading from '../Loading/Loading';
-import { Random } from 'meteor/random';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+  Random,
+} from 'meteor/random';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+} from 'react-beautiful-dnd';
 
-import './EditSchemaForm.scss'
-
-const styles = {
-
-};
+import './EditSchemaForm.scss';
 
 // fake data generator
 const getItems = count =>
-  Array.from({ length: count }, (v, k) => k).map(k => ({
+  Array.from({
+    length: count,
+  }, (v, k) => k).map(k => ({
     id: `item-${k}`,
     content: `Form Field ${k}`,
   }));
 
 const getItems2 = count =>
-  Array.from({ length: count }, (v, k) => k).map(k => ({
+  Array.from({
+    length: count,
+  }, (v, k) => k).map(k => ({
     id: `item2-${k}`,
     content: `item2 ${k}`,
   }));
@@ -60,10 +65,6 @@ export default class EditSchemaForm extends React.Component {
 
   }
 
-  handleSubmit() {
-
-  }
-
   onDragEnd(result) {
     // dropped outside the list
     if (!result.destination) {
@@ -77,58 +78,56 @@ export default class EditSchemaForm extends React.Component {
       const items = reorder(
         this.state[result.source.droppableId],
         result.source.index,
-        result.destination.index
+        result.destination.index,
       );
 
       this.setState({
-        [result.source.droppableId]: [...items]
+        [result.source.droppableId]: [...items],
       });
     } else if (result.destination.droppableId === 'droppable3') {
-
       // Original Droppable state doesn't have item removed. Added to droppable 3.
 
       const getNewLists = () => {
-        const removedResult = [...this.state[result.source.droppableId]]
-        const addedResult = [...this.state[result.destination.droppableId]]
+        const removedResult = [...this.state[result.source.droppableId]];
+        const addedResult = [...this.state[result.destination.droppableId]];
 
-        const [removedItem] = removedResult.splice(result.source.index, 1)
-        const newItem = { ...removedItem };
-        newItem.id = `${newItem.id}-${Random.id(5)}`
-        addedResult.splice(result.destination.index, 0, newItem)
+        const [removedItem] = removedResult.splice(result.source.index, 1);
+        const newItem = {
+          ...removedItem,
+        };
+        newItem.id = `${newItem.id}-${Random.id(5)}`;
+        addedResult.splice(result.destination.index, 0, newItem);
         return {
           removedResult,
-          addedResult
-        }
-      }
+          addedResult,
+        };
+      };
 
       this.setState({
-        [result.destination.droppableId]: [...getNewLists().addedResult]
+        [result.destination.droppableId]: [...getNewLists().addedResult],
       });
-
     } else if (result.source.droppableId === 'droppable3') {
 
       // Cannot drag from droppable3 right now, no change.
 
     } else {
       const getNewLists = () => {
-        const removedResult = [...this.state[result.source.droppableId]]
-        const addedResult = [...this.state[result.destination.droppableId]]
+        const removedResult = [...this.state[result.source.droppableId]];
+        const addedResult = [...this.state[result.destination.droppableId]];
 
-        const [removedItem] = removedResult.splice(result.source.index, 1)
-        addedResult.splice(result.destination.index, 0, removedItem)
+        const [removedItem] = removedResult.splice(result.source.index, 1);
+        addedResult.splice(result.destination.index, 0, removedItem);
         return {
           removedResult,
-          addedResult
-        }
-      }
+          addedResult,
+        };
+      };
 
       this.setState({
         [result.source.droppableId]: [...getNewLists().removedResult],
-        [result.destination.droppableId]: [...getNewLists().addedResult]
+        [result.destination.droppableId]: [...getNewLists().addedResult],
       });
-
     }
-
   }
 
   render() {
@@ -136,67 +135,67 @@ export default class EditSchemaForm extends React.Component {
       <DragDropContext onDragEnd={this.onDragEnd}>
         <div className="edit-schema-form">
 
-            <Droppable droppableId="droppable" className="edit-schema-form-left">
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  className="tableComponentsList"
-                >
-                  {this.state.droppable.map(item => (
-                    <Draggable key={item.id} draggableId={item.id}>
-                      {(provided, snapshot) => (
-                        <div>
-                          <div
-                            ref={provided.innerRef}
-                            style={getItemStyle(
-                              provided.draggableStyle,
-                              snapshot.isDragging
-                            )}
-                            className="draggableComponents"
-                            {...provided.dragHandleProps}
-                          >
-                            {item.content}
-                          </div>
-                          {provided.placeholder}
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-            <Droppable droppableId="droppable3" className="edit-schema-form-right">
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  className={(snapshot.isDraggingOver) ? "innerDroppable-hover" : "innerDroppable"}
-                >
-                  {this.state.droppable3.map(item => (
-                    <Draggable key={item.id} draggableId={item.id}>
-                      {(provided, snapshot) => (
-                        <div>
-                          <div
-                            ref={provided.innerRef}
-                            style={getItemStyle(
+          <Droppable droppableId="droppable" className="edit-schema-form-left">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                className="tableComponentsList"
+              >
+                {this.state.droppable.map(item => (
+                  <Draggable key={item.id} draggableId={item.id}>
+                    {(provided, snapshot) => (
+                      <div>
+                        <div
+                          ref={provided.innerRef}
+                          style={getItemStyle(
                               provided.draggableStyle,
                               snapshot.isDragging,
                             )}
-                            className="droppedComponents"
-                            {...provided.dragHandleProps}
-                          >
-                            {item.content}
-                          </div>
-                          {provided.placeholder}
+                          className="draggableComponents"
+                          {...provided.dragHandleProps}
+                        >
+                          {item.content}
                         </div>
+                        {provided.placeholder}
+                      </div>
                       )}
-                    </Draggable>
+                  </Draggable>
                   ))}
-                  {provided.placeholder}
-                </div>
+                {provided.placeholder}
+              </div>
               )}
-            </Droppable>
-      </div>
+          </Droppable>
+          <Droppable droppableId="droppable3" className="edit-schema-form-right">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                className={(snapshot.isDraggingOver) ? "innerDroppable-hover" : "innerDroppable"}
+              >
+                {this.state.droppable3.map(item => (
+                  <Draggable key={item.id} draggableId={item.id}>
+                    {(provided, snapshot) => (
+                      <div>
+                        <div
+                          ref={provided.innerRef}
+                          style={getItemStyle(
+                              provided.draggableStyle,
+                              snapshot.isDragging,
+                            )}
+                          className="droppedComponents"
+                          {...provided.dragHandleProps}
+                        >
+                          {item.content}
+                        </div>
+                        {provided.placeholder}
+                      </div>
+                      )}
+                  </Draggable>
+                  ))}
+                {provided.placeholder}
+              </div>
+              )}
+          </Droppable>
+        </div>
       </DragDropContext>
     );
   }
